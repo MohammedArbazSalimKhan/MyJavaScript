941b7fa22b353feaa1c66b8a80c8563fa53109df
Mohammed Arbaz Salim Khan <80943210+MohammedArbazSalimKhan@users.noreply.github.com>
2025-10-16 18:04:06 +0000

    changes

---
+++ 03_BasicFunction/01_function.js
@@ -0,0 +1,18 @@
+function sayMyName(){
+    console.log("A")
+    console.log("r")
+    console.log("b")
+    console.log("a")
+    console.log("z")
+    
+}
+
+sayMyName()
+
+
+
+function calculatePrice(...num){
+    return num
+}
+
+console.log(calculatePrice(1,2,3,4,5,100,6))
